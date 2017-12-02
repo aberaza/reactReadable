ba15c07750071afcac10636c633a22937f723e26
Aritz Beraza <aritz.beraza@gmail.com>
2017-12-02 19:45:49 +0100

    feat:support for server api

---
+++ src/utils/blogAPI.js
@@ -0,0 +1,82 @@
+const api = "localhost:3001"
+// Generate a unique token for storing your bookshelf data on the backend server.
+let token = localStorage.token
+if (!token){
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+}
+
+const headers = {
+    'Accept': 'application/json',
+    'Authorization': token
+}
+
+export const getCategories = () =>
+    fetch(`${api}/categories`, { headers })
+        .then(res => res.json())
+
+export const getCategoryPosts = (category) =>
+    fetch(`${api}/${category}/posts`, { headers })
+        .then(res => res.json())
+
+export const getPosts = () => 
+    fetch(`${api}/posts`, { headers })
+        .then(res => res.json())
+
+export const addPost = ({id, timestamp, title, body, author, category}) => 
+    fetch(`${api}/posts`, { metod:'POST', 
+        headers : {...headers, 'Content-Type' : 'application/json' },
+        body : JSON.stringify({id, timestamp, title, body, author, category}) })
+        .then(res => res.json())
+
+export const getPost = ( id ) =>
+    fetch(`${api}/posts/${id}`, { headers })
+        .then(res => res.json())
+
+// option can be 'upVote' or 'downVote'
+export const votePost = (id, option='upVote') =>
+    fetch(`${api}/posts/${id}`,
+            {method:'POST', headers : {...headers, 'Content-Type':'application/json'}, body:JSON.stringify({option})})
+        .then(res => res.json())
+
+export const editPost = (id, {title, body}) => 
+    fetch(`${api}/posts/${id}`,
+            {method:'PUT', headers : {...headers, 'Content-Type':'application/json'}, body:JSON.stringify({title, body})})
+        .then(res => res.json())
+
+export const deletePost = ( id ) =>
+    fetch(`${api}/posts/${id}`, {method:'DELETE', headers})
+        .then(res => res.json())
+
+export const getPostComments = ( id ) =>
+    fetch(`${api}/posts/${id}/comments`, { headers })
+        .then(res => res.json())    
+
+export const getComment = ( id ) => 
+    fetch(`${api}/comments/${id}`, { headers })
+        .then(res => res.json())
+
+export const addComment = ({id, timestamp, body, author, parentId}) =>
+    fetch(`${api}/comments`,
+            {method:'POST', headers : {...headers, 'Content-Type':'application/json'}, body:JSON.stringify({id, timestamp, body, author, parentId})})
+        .then(res => res.json())
+
+// option can be 'upVote' or 'downVote'
+export const voteComment = (id, option='upVote') =>
+    fetch(`${api}/comments/${id}`,
+            {method:'POST', headers : {...headers, 'Content-Type':'application/json'}, body:JSON.stringify({option})})
+        .then(res => res.json())
+
+export const editComment = (id, {timestamp, body}) => 
+    fetch(`${api}/comments/${id}`,
+            {method:'PUT', headers : {...headers, 'Content-Type':'application/json'}, body:JSON.stringify({timestamp, body})})
+        .then(res => res.json())
+
+export const deleteComment = ( id ) =>
+    fetch(`${api}/comments/${id}`, {method:'DELETE', headers})
+        .then(res => res.json())
+
+
+/* 
+    fetch(`${api}/`, { headers })
+        .then(res => res.json())
+*/
