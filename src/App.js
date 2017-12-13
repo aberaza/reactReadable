a8c6ff5ca16db0196990b6c9959d6d36008943a5
Aritz Beraza <aritz.beraza@gmail.com>
2017-12-13 11:10:47 +0100

    fix: direct access to add post or comment displays error
    Fixed error in which direct access to the url to add a comment or post displayed a Materializecss error.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { AppPostEdit } from './components/postEdit'
 
 
 import 'materialize-css/dist/css/materialize.min.css'
-import 'jquery/dist/jquery.min.js'
+//import 'jquery/dist/jquery.min.js'
 import 'materialize-css/dist/js/materialize.min.js'
 
 class App extends Component {
@@ -39,7 +39,8 @@ class App extends Component {
         <div className="fixed-action-btn horizontal">
           <Switch>
             <Route exact path="/:category/:id" render={({location})=>( <Link to={`${location.pathname}/comment`} id="addBtn" className="btn-floating btn-large green modal-trigger"><i className="large material-icons pink">comment</i></Link> )} />
-            <Route path="/new" render={_=>(<span></span>)} />
+            <Route exact path="/:category/:id/comment" render={({location})=>( <Link to={`${location.pathname}/comment`} id="addBtn" className="btn-floating btn-large green modal-trigger disabled"><i className="large material-icons pink">comment</i></Link> )} />
+            <Route path="/new" render={_=>(<a href="#!" id="addBtn" className="btn-floating btn-large blue modal-trigger disabled"><i className="large material-icons">add</i></a> )} />
             <Route exact path="/" render={_=>(<Link to="/new" id="addBtn" className="btn-floating btn-large green modal-trigger"><i className="large material-icons">add</i></Link>)} />
             <Route exact path="/:category" render={_=>(<Link to="/new" id="addBtn" className="btn-floating btn-large green modal-trigger"><i className="large material-icons">add</i></Link>)} />
           </Switch>
